7f91580febfb89587d0fc12b6df9aa6b1d3b9eed
Marcos <80154421+M4rdom@users.noreply.github.com>
2024-10-14 15:26:06 +0200

    Updated Links

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,13 +1,13 @@
 export const environment = {
     PRODUCTION: false,
     // Repository Manager
-    REPOSITORY_MANAGER_URL: 'http://localhost:5000',
+    REPOSITORY_MANAGER_URL: 'http://localhost:5000/repository-manager',
         FEATURE_MODEL: 'FeatureModel',
         VERSIONS:'Versions',
         LIST_TEMPLATES: 'ListVersions',
     
     // UVEngine Resolver
-    UVENGINE_RESOLVER_URL: 'http://localhost:5001',
+    UVENGINE_RESOLVER_URL: 'http://localhost:5001/uvengine-resolver',
         RESOLVE: 'Resolver',
         
     
